7916294f71b7c21b200a6b4da76668570a3c8aa0
Vivek Patil <117626322+vkpatil123@users.noreply.github.com>
2025-08-09 18:16:31 +0530

    Add files via upload
    settled with local storage 
    version 0.1

---
+++ all.js
@@ -0,0 +1,9 @@
+// // 1. Save the actual sheet data
+// localStorage.setItem(`sheet_${sheetName}`, JSON.stringify(sheetData));
+
+// // 2. Update the sheetList array
+// let list = JSON.parse(localStorage.getItem("sheetList")) || [];
+// if (!list.includes(sheetName)) {
+//   list.push(sheetName);
+//   localStorage.setItem("sheetList", JSON.stringify(list));
+// }
